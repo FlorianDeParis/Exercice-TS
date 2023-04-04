bc1017fe1a621e4736098817f533e8356c265f7e
Florian Tournay <florian.tournay@hotmail.fr>
2023-04-04 17:06:12 +0200

    dev: Add proxy config on vite

--- back/src/server.ts
+++ back/src/server.ts
@@ -5,13 +5,13 @@ import api from "./api";
 
 const app = express();
 const port = 3000;
-const publicDir = ".";
+const publicDir = "../front/dist/";
 
-app.use((req, res, next) => {
-  console.log("req: ", req.url);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log("req: ", req.url);
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   next();
+// });
 
 app.use("/api", api);
 
